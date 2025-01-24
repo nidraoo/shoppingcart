ad0196979cfcc10d970c081e4640f70c141d701c
nidraoo <05nidzzzzz@gmail.com>
2025-01-24 17:27:18 +0530

    new change

--- cart/src/components/ProductList.js
+++ cart/src/components/ProductList.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Product from './Product'
 
-export default function ProductList() {
+export default function ProductList(props) {
   return (
     <div>
-      
+      props.productlist.map((product) => {
+        return <Product product={product}/>
+      })
     </div>
   )
 }
