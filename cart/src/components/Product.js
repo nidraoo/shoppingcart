10fc1d682291fa17eb540f1a6eea43e9d18a711c
nidraoo <05nidzzzzz@gmail.com>
2025-01-24 20:54:10 +0530

    new change

--- cart/src/components/Product.js
+++ cart/src/components/Product.js
@@ -3,25 +3,27 @@ import React from "react";
 export default function Product(props) {
   return (
     <div className="row">
-      <div className="col-6">
+      <div className="col-5">
         <h2>
           {props.product.name}
           <span class="badge text-bg-secondary">₹{props.product.price}</span>
         </h2>
       </div>
-      <div className="col-4">
+      <div className="col-3">
         <div class="btn-group" role="group" aria-label="Basic outlined example">
           <button type="button" class="btn btn-outline-primary">
             -
           </button>
           <button type="button" class="btn btn-outline-primary">
-            Quantity
+            {props.product.quantity}
           </button>
           <button type="button" class="btn btn-outline-primary">
             +
           </button>
         </div>
-        Checkb
+      </div>
+      <div className="col-4">
+        {props.product.quantity*props.product.price}
       </div>
     </div>
   );
