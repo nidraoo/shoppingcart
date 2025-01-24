11e347ad8ec814221d07216912a07e576e393bc1
nidraoo <05nidzzzzz@gmail.com>
2025-01-24 17:08:55 +0530

    new change

--- cart/src/App.js
+++ cart/src/App.js
@@ -4,6 +4,19 @@ import Navbar from './components/navbar';
 import ProductList from './components/ProductList';
 
 function App() {
+  const product= [
+    {
+      price:99999,
+      name: "Iphone",
+      quantity:0,
+    },{
+      price:9999,
+      name:"redmi",
+      quantity:0,
+    },{
+
+    }
+  ]
   return (
     <>
       <Navbar/>
