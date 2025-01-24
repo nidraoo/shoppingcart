28e712653df85a5be1c7fbf907ece5c495b9b6c0
nidraoo <05nidzzzzz@gmail.com>
2025-01-24 21:13:06 +0530

    new change

--- cart/src/components/ProductList.js
+++ cart/src/components/ProductList.js
@@ -5,7 +5,7 @@ export default function ProductList(props) {
   return (
     <div>
     {props.productlist.map((product, i) => {
-    return <Product product={product} key={i} />;
+    return <Product product={product} key={i} incrementQuantity={props.incrementQuantity} index={i} />;
   })}
 </div>
 
