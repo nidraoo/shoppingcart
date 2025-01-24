6556d794bdfa67a8f0f0388121e1b15c99a77a3c
nidraoo <05nidzzzzz@gmail.com>
2025-01-24 19:43:49 +0530

    new change

--- cart/src/components/Product.js
+++ cart/src/components/Product.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Product() {
   return (
-    <div>
+    <div className='row'>
+        <div className='col-6'>
+            <h2>{props.product.name}</h2>
+        </div>
       
     </div>
   )
--- cart/src/components/ProductList.js
+++ cart/src/components/ProductList.js
@@ -4,7 +4,7 @@ import Product from './Product'
 export default function ProductList(props) {
   return (
     <div>
-  {props.productlist.map((product, i) => {
+    {props.productlist.map((product, i) => {
     return <Product product={product} key={i} />;
   })}
 </div>
